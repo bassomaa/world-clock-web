4294b7346ba35c6416a90981d3465f7060505689
Bassma <Bassmacherfi@gmail.com>
2025-07-18 01:17:11 +0200

    added css and javascript ot the project

--- src/app.js
+++ src/app.js
@@ -1,1 +1,46 @@
+function showDate(selection) {
+  let cities = document.querySelector("#cities-shown");
+  console.log(cities);
+  cities.innerHTML = "";
+  let formattedDate = moment().tz(selection).format("dddd Do YYYY");
+  let formattedTime = moment().tz(selection).format("h:mm:ss");
+  let formattedDetail = moment().tz(selection).format("A");
+  let city = selection.split("/")[1].replace("_", "");
+  console.log(formattedDate);
+  console.log(formattedTime);
+  console.log(formattedDetail);
+  console.log(city);
+  cities.innerHTML = ` <div id="first-city">
+            <div>
+              <div class="city" id="America/Los_Angeles">${city}</div>
+              <div class="details">${formattedDate}</div>
+            </div>
+            <div class="time">${formattedTime} <span class="unit">${formattedDetail}</span></div>
+          </div>
+  `;
+}
+function updateData(id, timezone) {
+  let container = document.querySelector(`#${id}`);
+  let dateElement = container.querySelector(".details");
+  let timeElement = container.querySelector(".time");
 
+  let defineTime = moment().tz(timezone);
+  let formatDate = defineTime.format("dddd Do YYYY");
+  let formatTime = defineTime.format("h:mm:ss");
+  let formatUnit = defineTime.format("A");
+  console.log(formatUnit);
+  dateElement.innerHTML = formatDate;
+  timeElement.innerHTML = ` <div class="time">${formatTime} <span class="unit">${formatUnit}</span></div>`;
+}
+function showTimeCity(event) {
+  let userSelection = event.target.value;
+  if (userSelection === "my-location") {
+    userSelection = moment.tz.guess();
+  }
+  showDate(userSelection);
+}
+
+let dropDownSelect = document.querySelector("#cities");
+dropDownSelect.addEventListener("change", showTimeCity);
+setInterval(() => updateData("first-city", "America/Los_Angeles"), 1000);
+setInterval(() => updateData("second-city", "Asia/Tokyo"));
